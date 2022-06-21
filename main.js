6a5214f6164316b75fbc1824612560f6b703b627
Nikki X <djknitex@outlook.com>
2022-06-21 12:15:19 -0700

    working on magic feature

--- main.js
+++ main.js
@@ -28,32 +28,54 @@ document.getElementById('playerMP').innerHTML = `${player.mp} / ${player.mp}`;
 document.getElementById('playerMP').style = "width: 100%";
 
 
-
 function attack() {
-    if (monsterHP <= 0) {
-        document.getElementById("text").innerText = "You did it! You ate the pizza."
-        document.getElementById("text2").innerText = ""
-        alert("You finished the alpha, go play some other game!")
-    } else {
         let random = Math.floor(Math.random() * 10)
-        if (random >= 5) {
-            monsterHP = monsterHP - 1;
-            document.getElementById('monsterHP').innerHTML = `${monsterHP} / ${monster.hp}`;
-            monsterWidth = monsterWidth - 20;
-            document.getElementById('monsterHP').style = `width: ${monsterWidth}%`
-            document.getElementById("text").innerText = "You hit the pizza!"
+        if (random >= 5 ) {
+                monsterHP = monsterHP - 1;
+                document.getElementById('monsterHP').innerHTML = `${monsterHP} / ${monster.hp}`;
+                monsterWidth = monsterWidth - 20;
+                document.getElementById('monsterHP').style = `width: ${monsterWidth}%`
+                document.getElementById("text").innerText = "You hit the pizza!"
+                if (monsterHP <= 0) {
+                    document.getElementById("text").innerText = "You ate the pizza!"
+                    document.getElementById("pizza").removeAttribute('src')
+                    document.getElementById("monster").textContent = 'You did it!'
+                    document.getElementById('attack').setAttribute('onclick','done()')
+    
+                }
         } else {
-
-            if (playerHP <= 0) {
-                alert("You dead. Go play some other game")
-            } else {
-                document.getElementById("text").innerText = "You miss!"
-                playerHP = playerHP - 1
-                document.getElementById('playerHP').innerHTML = `${playerHP} / ${player.hp}`;
-                playerWidth = playerWidth - 10;
-                document.getElementById('playerHP').style = `width: ${playerWidth}%`
-                document.getElementById("text2").innerText = "Space Pizza hit you for 1 damage!"
-            }
+                    document.getElementById("text").innerText = "You miss!"
+                    playerHP = playerHP - 1
+                    document.getElementById('playerHP').innerHTML = `${playerHP} / ${player.hp}`;
+                    playerWidth = playerWidth - 10;
+                    document.getElementById('playerHP').style = `width: ${playerWidth}%`
+                    document.getElementById("text").innerText = "Space Pizza hit you for 1 damage!"
+                    if (playerHP <= 0) {
+                        document.getElementById('attack').setAttribute('onclick','done()')
+                        document.getElementById("text").innerText = "You're gone. Go play another game."
+                        document.getElementById("monster").textContent = 'The Pizza was too much for you'
+                    }
         }
     }
+
+
+function done() {
+    if (monsterHP <= 0) {
+        alert("You already ate the pizza. Go play something else!")
+    } else if (playerHP <= 0 ) {
+        alert("You done messed up. Go play something else.")
+    }
+}
+
+function magic() {
+    var checkedValue = document.querySelector('.form-check-input:checked').value;
+    monsterHP = monsterHP - 2;
+    document.getElementById('monsterHP').innerHTML = `${monsterHP} / ${monster.hp}`;
+    monsterWidth = monsterWidth - 40;
+    document.getElementById('monsterHP').style = `width: ${monsterWidth}%`
+
+}
+
+function nextModal() {
+    console.log("test")
 }
